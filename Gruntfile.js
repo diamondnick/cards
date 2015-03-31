b90f1efb8fd6ec6b6ef980bf16faf64c34211fb2
Nick Diamond <Nick.Diamond@csaa.com>
2015-03-31 08:37:06 -0700

    -a

--- Gruntfile.js
+++ Gruntfile.js
@@ -245,7 +245,7 @@ exec: {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+         // '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
